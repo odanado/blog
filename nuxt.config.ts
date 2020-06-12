4184855dc253a2dd89d971293dbf2c9adb8f14d4
odan <odan3240@gmail.com>
2020-06-13 05:42:38 +0900

    chore: add nuxt-ts

---
+++ nuxt.config.ts
@@ -0,0 +1,63 @@
+import { getConfig } from './config';
+const config = getConfig();
+
+/** @type {import('@nuxt/types').Configuration} */
+export default {
+  server: {
+    port: config.port,
+    host: config.host
+  },
+  mode: 'universal',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    titleTemplate: '%s - odan blog',
+    htmlAttrs: {
+      lang: 'ja'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    '@exampledev/new.css/new.css'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    '~/plugins/repository'
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    '@nuxt/typescript-build',
+    // Doc: https://github.com/nuxt-community/stylelint-module
+    '@nuxtjs/stylelint-module'
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    '@nuxt/content'
+  ],
+  /*
+  ** Build configuration
+  */
+  build: {
+  }
+};
