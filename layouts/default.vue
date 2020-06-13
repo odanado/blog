a87cae53a162661e1d128953540c4eb849dd1b56
odan <odan3240@gmail.com>
2020-06-13 21:37:16 +0900

    feat: add canonical link

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,11 +7,22 @@
 
 <script lang="ts">
 import Vue from 'vue';
+import { MetaInfo } from 'vue-meta';
 import TheHeader from '../components/the-header.vue';
 
 export default Vue.extend({
   components: {
     TheHeader
+  },
+  head (): MetaInfo {
+    const path = this.$route.path.endsWith('/') ? this.$route.path.slice(0, -1) : `${this.$route.path}`;
+    const canonical = `${this.$config.origin}${path}`;
+    const link = [
+      { rel: 'canonical', href: canonical }
+    ];
+    return {
+      link
+    };
   }
 });
 </script>
