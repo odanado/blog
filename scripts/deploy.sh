a44f4a0301accf8a57d6a478fc52b5892f97b636
odan <odan3240@gmail.com>
2020-07-07 05:13:27 +0900

    chore: add Cache-Control header to static files

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,4 +6,6 @@ S3_BUCKET=blog.odan.dev
 CLOUDFRONT_DISTRIBUTION_ID=ECT72K2AV2HMN
 
 aws s3 cp --recursive ./dist s3://$S3_BUCKET
+aws s3 cp --recursive ./dist s3://$S3_BUCKET --exclude "*" --include="*.html" --metadata="Cache-Control='public, max-age=0, must-revalidate'"
+aws s3 cp --recursive ./dist s3://$S3_BUCKET --exclude "*" --include="*.js" --metadata="Cache-Control='public, max-age=86400'"
 aws cloudfront create-invalidation --distribution-id $CLOUDFRONT_DISTRIBUTION_ID --paths "/*"
