c20799213ccd4398e5afa315e11c4a1a4bd88a67
odan <odan3240@gmail.com>
2020-08-17 02:08:32 +0900

    chore: setup google search console

--- terraform/domain.tf
+++ terraform/domain.tf
@@ -13,3 +13,11 @@ resource "aws_route53_record" "blog" {
     evaluate_target_health = true
   }
 }
+
+resource "aws_route53_record" "google_search_console" {
+  zone_id = data.aws_route53_zone.selected.zone_id
+  name    = local.domain
+  type    = "TXT"
+  ttl     = "3600"
+  records = ["google-site-verification=w2_cY-1YQcAq87zOZZhBegMk3L_hW-xGBg10fLKZnU8"]
+}
