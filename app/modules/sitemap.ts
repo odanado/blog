9f756c9a5992a845c727b8c99bd21dd53680315b
odan <odan3240@gmail.com>
2020-08-17 03:08:33 +0900

    fix: update sitemap module

--- app/modules/sitemap.ts
+++ app/modules/sitemap.ts
@@ -1,13 +1,14 @@
 import { defineNuxtModule } from '@nuxtjs/composition-api';
-import { ArticleRepository } from '../repositories/article.repository';
 
 export default defineNuxtModule(function () {
-  const { $content } = require('@nuxt/content');
-  const articleRepository = new ArticleRepository($content);
-
-  this.nuxt.hook('sitemap:generate:before', async (_: unknown, sitemapOptions: any) => {
-    const articles = await articleRepository.fetchArticles();
-    const paths = articles.map(article => article.path);
-    sitemapOptions[0].routes = [...(sitemapOptions[0].routes || []), ...paths];
+  const routes: string[] = [];
+  this.nuxt.hook('generate:route', ({ route }: { route: string}) => {
+    routes.push(route);
+  });
+  this.nuxt.hook('sitemap:generate:before', (_: unknown, sitemapOptions: any) => {
+    if (!sitemapOptions[0].routes) {
+      sitemapOptions[0].routes = [];
+    }
+    sitemapOptions[0].routes = [...sitemapOptions[0].routes, ...routes];
   });
 });
