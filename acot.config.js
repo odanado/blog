22adfe895b52449c06836c79f56e640e369645f1
odan <odan3240@gmail.com>
2021-01-03 07:40:31 +0900

    chore: run-acot を削除

--- acot.config.js
+++ acot.config.js
@@ -1,5 +1,11 @@
+// @ts-check
+
+/** @type {import('@acot/types').Config} */
 module.exports = {
   extends: ['@acot'],
+  connection: {
+    command: 'yarn http-server dist'
+  },
   origin: 'http://localhost:8080',
   paths: ['/']
 };
