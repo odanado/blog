9c037a3871755cd5ff08f34e41f0a6dc3f923e62
odan <odan3240@gmail.com>
2024-01-04 01:24:15 +0900

    run format

--- src/scripts/send-metrics-to-gcp.ts
+++ src/scripts/send-metrics-to-gcp.ts
@@ -21,41 +21,41 @@ type Result = {
 const getMetrics = (): Result => {
   const result: Result = {
     totalDependenciesCount: Number.parseInt(
-      process.env["TOTAL_DEPENDENCIES_COUNT"]!
+      process.env["TOTAL_DEPENDENCIES_COUNT"]!,
     ),
 
     outdatedDependenciesCount: Number.parseInt(
-      process.env["OUTDATED_DEPENDENCIES_COUNT"]!
+      process.env["OUTDATED_DEPENDENCIES_COUNT"]!,
     ),
     latestDependenciesCount: Number.parseInt(
-      process.env["LATEST_DEPENDENCIES_COUNT"]!
+      process.env["LATEST_DEPENDENCIES_COUNT"]!,
     ),
 
     outdatedDependenciesPercentage: Number.parseFloat(
-      process.env["OUTDATED_DEPENDENCIES_PERCENTAGE"]!
+      process.env["OUTDATED_DEPENDENCIES_PERCENTAGE"]!,
     ),
     latestDependenciesPercentage: Number.parseFloat(
-      process.env["LATEST_DEPENDENCIES_PERCENTAGE"]!
+      process.env["LATEST_DEPENDENCIES_PERCENTAGE"]!,
     ),
 
     outdatedMajorDependenciesCount: Number.parseInt(
-      process.env["OUTDATED_MAJOR_DEPENDENCIES_COUNT"]!
+      process.env["OUTDATED_MAJOR_DEPENDENCIES_COUNT"]!,
     ),
     outdatedMinorDependenciesCount: Number.parseInt(
-      process.env["OUTDATED_MINOR_DEPENDENCIES_COUNT"]!
+      process.env["OUTDATED_MINOR_DEPENDENCIES_COUNT"]!,
     ),
     outdatedPatchDependenciesCount: Number.parseInt(
-      process.env["OUTDATED_PATCH_DEPENDENCIES_COUNT"]!
+      process.env["OUTDATED_PATCH_DEPENDENCIES_COUNT"]!,
     ),
 
     outdatedMajorDependenciesPercentage: Number.parseFloat(
-      process.env["OUTDATED_MAJOR_DEPENDENCIES_PERCENTAGE"]!
+      process.env["OUTDATED_MAJOR_DEPENDENCIES_PERCENTAGE"]!,
     ),
     outdatedMinorDependenciesPercentage: Number.parseFloat(
-      process.env["OUTDATED_MINOR_DEPENDENCIES_PERCENTAGE"]!
+      process.env["OUTDATED_MINOR_DEPENDENCIES_PERCENTAGE"]!,
     ),
     outdatedPatchDependenciesPercentage: Number.parseFloat(
-      process.env["OUTDATED_PATCH_DEPENDENCIES_PERCENTAGE"]!
+      process.env["OUTDATED_PATCH_DEPENDENCIES_PERCENTAGE"]!,
     ),
   };
 
